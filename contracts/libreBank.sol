214129df7c8338aeeb2690d6dbac14441cd4316c
Oleg <berser@yandex.ru>
2017-10-09 22:35:17 +0700

    test

---
+++ contracts/libreBank.sol
@@ -0,0 +1,187 @@
+pragma solidity ^0.4.10;
+
+import "./zeppelin/lifecycle/Pausable.sol";
+import "./zeppelin/math/SafeMath.sol";
+
+
+interface token {
+    /*function transfer(address receiver, uint amount);*/
+    function balanceOf(address _owner) returns (uint256);
+    function mint(address _to,uint256 _amount);
+    function getTokensAmount() public returns(uint256);
+}
+
+interface oracleInterface {
+    function update();
+    function getName() constant returns(string);
+}
+
+contract libreBank is Ownable,Pausable {
+    using SafeMath for uint256;
+    
+    enum limitType { minUsdRate, maxUsdRate, minTransactionAmount, minSellSpread, maxSpread,minBuySpread,maxBuySpread }
+    event newPriceTicker(string oracleName, string price);
+
+    /*
+    event LogSell(address Client, uint256 sendTokenAmount, uint256 EtherAmount, uint256 totalSupply);
+    event LogBuy(address Client, uint256 TokenAmount, uint256 sendEtherAmount, uint256 totalSupply);
+    event LogWhithdrawal (uint256 EtherAmount, address addressTo, uint invertPercentage);
+    */
+
+    struct oracleData {
+        string name;
+        address oracleAddress; // Maybe replace it on mapping (see below)
+        uint256 rating;
+        bool enabled;
+    }
+
+    oracleData[] oracles;
+    uint256 updateDataRequest;
+    
+    mapping (string=>address) oraclesAddress;
+
+ 
+    uint256 public currencyUpdateTime;
+    uint256 public ethUsdRate = 30000; // In $ cents
+    uint256[] limits;
+    oracleInterface currentOracle;
+    token libreToken;
+
+    function setLimitValue(limitType limitName, uint256 value) internal {
+        limits[uint(limitName)] = value;
+    }
+
+    function getLimitValue(limitType limitName )internal returns (uint256) {
+        return limits[uint(limitName)];
+    }
+
+    function getMinTransactionAmount() constant external returns(uint256) {
+        return getLimitValue(limitType.minTransactionAmount);
+    }
+    
+    function setMinTransactionAmount(uint256 amountInWei) onlyOwner {
+        setLimitValue(limitType.minTransactionAmount,amountInWei);
+    }
+
+    function setBuySpreadLimits(uint256 minBuySpread, uint256 minBuySpread) onlyOwner {
+        setLimitValue(limitType.minBuySpread,minSpead);
+        setLimitValue(limitType.maxBuySpread,maxSpread);
+        
+    }
+
+    function setSellSpreadLimits(uint256 minSellSpread, uint256 maxSellSpread) onlyOwner {
+        setLimitValue(limitType.minSellSpread,minSellSpread);
+        setLimitValue(limitType.maxSellSpread,maxSellSpread);
+    }
+
+    function setSpread(uint256 _buySpread,uint256 _sellSpread) onlyOnwer {
+        bool isBuyInLimit = buySpread > getLimitValue(limitType.minBuySpread) && buySpread < getLimitValue(limitType.maxBuySpread);
+        bool isSellInLimit = sellSpread > getLimitValue(limitType.minSellSpread) && sellSpread < getLimitValue(limitType.maxSellSpread);
+        require(isBuyInLimit && isSellInLimit);
+        buySpread = _buySpread;
+        sellSpread = _sellSpread;
+    }
+
+    function addOracle(address oracleAddress) onlyOwner {
+        require(oracleAddress != 0x0);
+        oracleInterface(oracleAddress);
+        oracleData memory thisOracle = new oracleData(oracleInterface.getName(),oracleAddress,0,true);
+        oracles.push(thisOracle);
+    }
+    function getOracleName(uint number) public constant returns(string) {
+        return oracles[number].name;
+    }
+    
+    // Ограничие на периодичность обновления курса - не чаще чем раз в 5 минут
+    modifier needUpdate() {
+        require(!isRateActual());
+        _;
+    }
+
+    function isRateActual() public constant returns(bool) {
+        return now <= currencyUpdateTime + 5 minutes;
+    }
+
+    function libreBank(address coinsContract) {
+        libreToken = token(coinsContract);
+    }
+    
+    function donate() payable {}
+
+    function getTokenPrice() returns(uint256) {
+        // Implement price calc logic later
+        uint256 tokenPrice = 100; // In $ cent
+        return tokenPrice;
+    }
+
+    function () payable {
+        buyTokens(msg.sender);
+    }
+
+    function setTokenToSell(address tokenAddress) onlyOwner {
+        libreToken = token(tokenAddress);
+    }
+
+    function totalTokens() returns (uint256) {
+        return libreToken.getTokensAmount();
+    }
+
+
+    function setCurrencyRate(uint256 rate) onlyOwner {
+        bool validRate = rate > 0 && rate < getLimitValue(limitType.maxUsdRate) && rate > getLimitValue(limitType.minUsdRate);
+        require(validRate);
+        ethUsdRate = rate;
+        currencyUpdateTime = now;
+    }
+
+    function withdrawEther(address beneficiar) onlyOwner {
+        beneficiar.send(this.balance);
+    }
+
+
+    function updateRate() needUpdate {
+        ethUsdRate = getRate();
+    }
+
+    function getRate() private returns(bool) {
+        uint256[] oracleResults;
+        for (uint256 i = 0; i < oracles.length; i++) {
+            if (oracles[i].enabled) oracleInterface(oracle[i].oracleAddress).update();
+        }
+        return true;
+    }
+
+    function oraclesCallback(string name,uint256 value,uint256 timestamp) {
+        // Implement it later
+    }
+
+
+
+    function buyTokens(address benificiar) {
+        require(msg.value > getLimitValue(limitType.minTransactionAmount));
+
+        uint256 tokensAmount = msg.value.mul(ethUsdRate).div(getTokenPrice());
+        libreToken.mint(benificiar,tokensAmount);
+    }
+
+    // ***************************************************************************
+
+    // Buy token by sending ether here
+    //
+    // Price is being determined by the algorithm in recalculatePrice()
+    // You can also send the ether directly to the contract address   
+    
+    function buy() payable external ExchangeActive {
+        require (msg.value > minEtherAmount);
+        buyLimit (0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF); 
+    }
+    
+    function buyLimit (uint _limit) payable public  ExchangeActive {
+        require (msg.value > minEtherAmount);
+        update(0, 1, msg.sender, msg.value, _limit);
+    }
+    // ! Not Impemented Yet
+    function sellTokens() {}
+}
+
+
