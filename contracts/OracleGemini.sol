00927d364a1e8f4a1dd267774cf1e67d76cb9cf1
evercodex <evercodex@gmail.com>
2017-10-18 13:35:33 +0300

    Gemini oracle

---
+++ contracts/OracleGemini.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.11;
+
+import "./zeppelin/ownership/Ownable.sol";
+import "./OracleBase.sol";
+
+contract OracleKraken is OracleBase {
+    bytes32 constant ORACLE_NAME = "Gemini Oraclize Async";
+    bytes16 constant ORACLE_TYPE = "ETHUSD";
+    string constant ORACLE_DATASOURCE = "URL";
+    string constant ORACLE_ARGUMENTS = "json(https://api.gemini.com/v1/pubticker/ethusd).ask";
+    
+    function OracleKraken(address _bankContract) public {
+        oracleName = ORACLE_NAME;
+        oracleType = ORACLE_TYPE;
+        oracleConfig = OracleConfig({datasource: ORACLE_DATASOURCE, arguments: ORACLE_ARGUMENTS});
+        bankContractAddress = _bankContract;
+        updateCosts();
+        //update();
+    }
+     
+    function donate() payable { }
+}
