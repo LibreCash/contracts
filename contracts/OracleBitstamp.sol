d1ce8ddbaeb8e0cece31cc997d18261215056236
evercodex <evercodex@gmail.com>
2017-10-18 13:24:38 +0300

    Bitstamp oracle

---
+++ contracts/OracleBitstamp.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.11;
+
+import "./zeppelin/ownership/Ownable.sol";
+import "./OracleBase.sol";
+
+contract OracleKraken is OracleBase {
+    bytes32 constant ORACLE_NAME = "Bitstamp Oraclize Async";
+    bytes16 constant ORACLE_TYPE = "ETHUSD";
+    string constant ORACLE_DATASOURCE = "URL";
+    string constant ORACLE_ARGUMENTS = "json(https://www.bitstamp.net/api/v2/ticker/ethusd).ask";
+    
+    function OracleKraken(address _bankContract) public {
+        oracleName = ORACLE_NAME;
+        oracleType = ORACLE_TYPE;
+        oracleConfig = OracleConfig({datasource: ORACLE_DATASOURCE, arguments: ORACLE_ARGUMENTS});
+        bankContractAddress = _bankContract;
+        updateCosts();
+        //update();
+    }
+     
+    function donate() payable { }
+}
