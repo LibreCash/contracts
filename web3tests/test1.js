b66a48854a1ae49a5448012d237144b2d2d872f7
evercodex <evercodex@gmail.com>
2017-11-04 17:07:03 +0300

    дополнение к web3 - ожидание транзакции, рабочий тест

--- web3tests/test1.js
+++ web3tests/test1.js
@@ -1,5 +1,4 @@
-async function main() {
-    let  
+async function main() {  
         contract = web3.eth.contract(JSON.parse(contractABI)).at(contractAddress); 
     //let mainAccount = await web3.eth.getCoinbase(),
       //  mainAccBalance = await web3.eth.getBalance(mainAccount);
@@ -10,8 +9,19 @@ async function main() {
 //    console.log(`token ${token}`);
     console.log(contract);
     web3.eth.defaultAccount = web3.eth.coinbase;
-    var updateRatesTx = await contract.requestUpdateRates();
-    console.log(updateRatesTx);
+    var buyFee = await contract.buyFee.call();
+    console.log(buyFee.toNumber());
+    var setBuyFeeTX = await contract.setBuyFee(300);
+    var x = await web3.eth.getTransactionReceiptMined(setBuyFeeTX, 500);
+    console.log(x);
+    console.log(web3.eth.getTransaction(setBuyFeeTX));
+    console.log(web3.eth.getTransactionReceipt(setBuyFeeTX));
+    var buyFee = await contract.buyFee.call();
+    console.log(buyFee.toNumber());
+ /*   var updateRatesTx = await contract.requestUpdateRates();
+    console.log(web3.eth.getTransaction(updateRatesTx));
+    var numWaitingOracles = await contract.numWaitingOracles.call();
+    console.log(numWaitingOracles.toString(10));*/
 
 }
 
---
+++ web3tests/web3additions.js
@@ -0,0 +1,30 @@
+web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
+    var transactionReceiptAsync;
+    interval = interval ? interval : 500;
+    transactionReceiptAsync = function(txnHash, resolve, reject) {
+        try {
+            var receipt = web3.eth.getTransactionReceipt(txnHash);
+            if (receipt == null) {
+                setTimeout(function () {
+                    transactionReceiptAsync(txnHash, resolve, reject);
+                }, interval);
+            } else {
+                resolve(receipt);
+            }
+        } catch(e) {
+            reject(e);
+        }
+    };
+
+    if (Array.isArray(txnHash)) {
+        var promises = [];
+        txnHash.forEach(function (oneTxHash) {
+            promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
+        });
+        return Promise.all(promises);
+    } else {
+        return new Promise(function (resolve, reject) {
+                transactionReceiptAsync(txnHash, resolve, reject);
+            });
+    }
+};
