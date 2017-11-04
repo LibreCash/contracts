5633ee274842a4ae1ec20e0fca955a9f26dfdb07
evercodex <evercodex@gmail.com>
2017-11-04 15:10:46 +0300

    рабочая среда для тестов

--- web3tests/test1.js
+++ web3tests/test1.js
@@ -1,27 +1,17 @@
-/*alert(contractName);
-alert(contractAddress);
-alert(contractABI);
-*/
-const 
-    Web3 = require('web3');
-
-    console.log(Web3.providers);
-var 
-   // web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:8545")),
-    web3 = new Web3("ws://localhost:8545"),
-    contract = web3.eth.contract(JSON.parse(contractABI), contractAddress);
-
 async function main() {
     let  
-        mainAccount = await web3.eth.getCoinbase(),
-        mainAccBalance = await web3.eth.getBalance(mainAccount);
-    let token = await contract.methods.getToken().call();
+        contract = web3.eth.contract(JSON.parse(contractABI)).at(contractAddress); 
+    //let mainAccount = await web3.eth.getCoinbase(),
+      //  mainAccBalance = await web3.eth.getBalance(mainAccount);
+//    let token = await contract.methods.getToken().call();
         
-    console.log(`Основной аккаунт ${mainAccount}`);
-    console.log(`Баланс основного аккаунта ${mainAccBalance}`);
-    console.log(`token ${token}`);
-    
-
+//    console.log(`Основной аккаунт ${mainAccount}`);
+//    console.log(`Баланс основного аккаунта ${mainAccBalance}`);
+//    console.log(`token ${token}`);
+    console.log(contract);
+    web3.eth.defaultAccount = web3.eth.coinbase;
+    var updateRatesTx = await contract.requestUpdateRates();
+    console.log(updateRatesTx);
 
 }
 
