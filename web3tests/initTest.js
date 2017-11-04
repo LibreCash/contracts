508c8dc1cd9a13b8a43ecc2c29b3e41cfd90893e
evercodex <evercodex@gmail.com>
2017-11-04 19:50:08 +0300

    множественность разных функций в тесте

---
+++ web3tests/initTest.js
@@ -0,0 +1,18 @@
+var allTestFunctions=[];
+for(var i in this) {
+	if(((typeof this[i]).toString() == "function") && (this[i].toString().indexOf("native") == -1)) {
+        if (this[i].name.substr(0, 4) == "test") {
+            allTestFunctions.push(this[i].name);
+        }
+	}
+}
+var divFunctionList = document.getElementById("functionList");
+allTestFunctions.forEach(function(testFunction) {
+    let buttonTestFunction = document.createElement('button');
+    buttonTestFunction.classList.add("buttonTestFunction");
+    buttonTestFunction.classList.add("btn");
+    buttonTestFunction.classList.add("btn-success");
+    buttonTestFunction.innerText = testFunction;
+    divFunctionList.appendChild(buttonTestFunction);
+    buttonTestFunction.onclick = window[testFunction];
+});
--- web3tests/tests/test1.js
+++ web3tests/tests/test1.js
@@ -1,13 +1,5 @@
-async function main() {  
+async function testFee() {  
     contract = web3.eth.contract(JSON.parse(contractABI)).at(contractAddress); 
-    //let mainAccount = await web3.eth.getCoinbase(),
-      //  mainAccBalance = await web3.eth.getBalance(mainAccount);
-//    let token = await contract.methods.getToken().call();
-        
-//    console.log(`Основной аккаунт ${mainAccount}`);
-//    console.log(`Баланс основного аккаунта ${mainAccBalance}`);
-//    console.log(`token ${token}`);
-//    console.log(contract);
     web3.eth.defaultAccount = web3.eth.coinbase;
     var buyFee = await contract.buyFee.call();
     console.log(buyFee.toNumber());
@@ -16,11 +8,4 @@ async function main() {
     console.log(web3.eth.getTransactionReceipt(setBuyFeeTXAddr));
     var buyFee = await contract.buyFee.call();
     console.log(buyFee.toNumber());
- /*   var updateRatesTx = await contract.requestUpdateRates();
-    console.log(web3.eth.getTransaction(updateRatesTx));
-    var numWaitingOracles = await contract.numWaitingOracles.call();
-    console.log(numWaitingOracles.toString(10));*/
-
 }
-
-main();
