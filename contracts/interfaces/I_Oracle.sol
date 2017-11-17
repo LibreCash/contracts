8964f1a1cb5742e4817c7ce64d7465f984092bf8
evercodex <evercodex@gmail.com>
2017-11-17 14:23:57 +0300

    working oracles and bank with no duplicating; changed interface to abstract contract

--- contracts/interfaces/I_Oracle.sol
+++ contracts/interfaces/I_Oracle.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.4.11;
 
-interface OracleI {
+contract OracleI {
     function updateRate() external returns (bool);
-    function name() constant public returns (bytes32);
+    //function name() constant public returns (bytes32);
+    bytes32 public name;
     function setBank(address _bankAddress) public;
-    function rate() public returns (uint256);
-    function queryId() public returns (bytes32);
+    //function rate() public returns (uint256);
+    uint256 public rate;
+    bytes32 public queryId;
+    //function queryId() public returns (bytes32);
     function clearState() public;
-    function updateTime() public returns (uint256);
+    //function updateTime() public returns (uint256);
+    uint256 public updateTime;
     //function hasReceivedRate() public view returns (bool);
 }
