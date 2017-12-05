e3f481132e00872876beacac38c5c074f8e9c0c5
decodemybrain <decodemybrain@protonmail.ch>
2017-12-05 11:05:31 +0300

    Добавил SafeMath и переписал её использованием.

---
+++ contracts/token/TokenSample.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.4.11;
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract ERC20 {
+    using SafeMath for uint256;
+    string public name = "Token Name";
+    string public symbol = "SYM";
+    uint8 public constant decimals = 18;
+    uint public totalSupply;
+
+    mapping(address => uint) balances;
+    mapping (address => mapping (address => uint)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    function ERC20(string tokenName, string tokenSymbol, uint emissionAmount) {
+        name = tokenName;
+        symbol = tokenSymbol;
+        totalSupply = emissionAmount * 10 ** uint(decimals);
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) constant returns (uint balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint _value) returns (bool) {
+        require(_to != address(0));
+        
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) returns (bool) {
+        var _allowance = allowed[_from][msg.sender];
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool) {
+        
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+        return allowed[_owner][_spender];
+    }
+}
