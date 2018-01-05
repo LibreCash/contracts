9f80efe0df97167f49c9aef56c027b46123f81ff
evercodex <evercodex@gmail.com>
2018-01-05 12:01:57 +0300

    LibertyToken

---
+++ contracts/token/LibertyToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.15;
+
+import "../zeppelin/token/StandardToken.sol";
+import "../zeppelin/ownership/Ownable.sol";
+
+
+contract LibertyToken is StandardToken, Ownable {
+    string public name = "Liberty Token";
+    string public symbol = "LBT";
+    uint256 public decimals = 18;
+
+    function LibertyToken() public {
+        totalSupply = 100 * (10**6) * (10**decimals);
+        balances[msg.sender] = totalSupply;
+    }
+}
