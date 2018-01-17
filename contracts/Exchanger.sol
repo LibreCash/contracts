3c88ae92496e0f85991af264cb1ac2f79c6cb1fa
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2018-01-17 18:57:43 +0300

    add exchanger without allowance

--- contracts/Exchanger.sol
+++ contracts/Exchanger.sol
@@ -2,39 +2,31 @@ pragma solidity ^0.4.11;
 
 import "./zeppelin/math/SafeMath.sol";
 import "./zeppelin/ownership/Ownable.sol";
+import "./zeppelin/token/StandardToken.sol";
+import "./zeppelin/token/BurnableToken.sol";
 
 
-contract ERC223BurnableInterface {
-    uint public totalSupply;
-    function balanceOf(address who) public constant returns (uint);
-    function transfer(address to, uint value) public;
-    function transfer(address to, uint value, bytes data) public;
-    function burn(uint256 _value) public;
-    event Transfer(address indexed from, address indexed to, uint value, bytes data);
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Burn(address indexed burner, uint256 value);
-}
-
+contract BurnableERC20 is BurnableToken, StandardToken {}
 
 /**
  * @title Token exchanger contract.
  *
- * @dev ERC223 token exchanger contract for an ICO.
+ * @dev ERC20 token exchanger contract for an ICO.
  */
 contract Exchanger is Ownable {
     using SafeMath for uint;
 
     address public supplyTokenAddress = 0x0;
     address public collectingTokenAddress = 0x0;
-    ERC223BurnableInterface private supplyToken;
-    ERC223BurnableInterface private collectingToken;
+    StandardToken private supplyToken;
+    BurnableERC20 private collectingToken;
     uint constant RATE_MULTIPLIER = 10**9;
     uint public rate = 10**9; // 10**9 / RATE_MULTIPLIER = 1
 
     event NewCollectingToken(address oldTokenContract, address newTokenContract);
     event NewRate(uint oldRate, uint newRate);
     event Deposit(address tokenSender, uint tokenAmount, address tokenContract);
-    event ExchangeRequest(address tokenSender, uint tokenAmount, bytes txData, address tokenContract);
+    event ExchangeRequest(address tokenSender, uint tokenAmount, address tokenContract);
     event SupplySent(address tokenBeneficiar, uint tokenAmount, address tokenContract);
 
     modifier tokens {
@@ -61,7 +53,7 @@ contract Exchanger is Ownable {
         require(_address != collectingTokenAddress);
         require(_address != 0x0);
         supplyTokenAddress = _address;
-        supplyToken = ERC223BurnableInterface(_address);
+        supplyToken = StandardToken(_address);
     }
 
     /**
@@ -73,7 +65,7 @@ contract Exchanger is Ownable {
         require(_address != supplyTokenAddress);
         NewCollectingToken(collectingTokenAddress, _address);
         collectingTokenAddress = _address;
-        collectingToken = ERC223BurnableInterface(_address);
+        collectingToken = BurnableERC20(_address);
     }
 
     /**
@@ -84,21 +76,21 @@ contract Exchanger is Ownable {
     }
 
     /**
-     * @dev Fallback; the place where exchanges are done.
-     * @param _from The address of token sender.
+     * @dev buySupplyToken; the place where exchanges are done.
      * @param _value The number of tokens.
-     * @param _data The tx data; not used here.
      */
-    function tokenFallback(address _from, uint _value, bytes _data) public tokens {
-        if (msg.sender == supplyTokenAddress) {
-            Deposit(_from, _value, msg.sender);
-            return;
-        }
-        ExchangeRequest(_from, _value, _data, msg.sender);
+    function buySupplyToken(uint _value) public {
+        require(_value <= collectingToken.allowance(msg.sender,this));
+        ExchangeRequest(msg.sender, _value, supplyTokenAddress);
         uint256 supplyToSend = _value.mul(rate) / RATE_MULTIPLIER;
-        require(supplyToSend <= supplyToken.balanceOf(this));
+
+        if (supplyToSend > supplyToken.balanceOf(this)) {
+            uint256 leesSupplyToken = supplyToken.balanceOf(this);
+            _value = leesSupplyToken.mul(RATE_MULTIPLIER) / rate;
+        }
+        collectingToken.transferFrom(msg.sender, this, _value);
         collectingToken.burn(_value);
-        supplyToken.transfer(_from, supplyToSend);
-        SupplySent(_from, supplyToSend, supplyTokenAddress);
+        supplyToken.transfer(msg.sender, supplyToSend);
+        SupplySent(msg.sender, supplyToSend, supplyTokenAddress);
     }
 }
