3da62ed4c446e65e7d10c44fab3eac7e9707171c
decodemybrain <decodemybrain@protonmail.ch>
2018-01-26 14:21:57 +0300

    Added JSDOC for contract

--- contracts/oracles/mock/OracleMockTest.sol
+++ contracts/oracles/mock/OracleMockTest.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.17;
 import "./OracleMockBase.sol";
 
+/**
+ * @title Mocked oracles contract for testing purposes.
+ */
 contract OracleMockTest is OracleMockBase {
     function OracleMockTest() {
         oracleName = "Test (Mocked Oracle, 1000)";
