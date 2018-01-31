84ed9f356fc410819484a2a73159b1d92e1e1e16
decodemybrain <decodemybrain@protonmail.ch>
2018-01-31 16:46:42 +0300

    Delete unused ownable dependency

--- contracts/token/LibertyToken.sol
+++ contracts/token/LibertyToken.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.4.17;
 
 import "../zeppelin/token/StandardToken.sol";
-import "../zeppelin/ownership/Ownable.sol";
 
 
-contract LibertyToken is StandardToken, Ownable {
-    string public name = "Liberty Token";
-    string public symbol = "LBT";
+contract LibertyToken is StandardToken {
+    string public name = "LibreBank";
+    string public symbol = "LBRS";
     uint256 public decimals = 18;
 
     function LibertyToken() public {
         totalSupply_ = 100 * (10**6) * (10**decimals);
         balances[msg.sender] = totalSupply_;
     }
-}
+}   
