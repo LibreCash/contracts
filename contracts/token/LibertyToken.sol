d12a3de0620f9a17e7a68746243f3575810918b8
decodemybrain <decodemybrain@protonmail.ch>
2018-01-31 17:30:22 +0300

    Add burning to LBRS token

--- contracts/token/LibertyToken.sol
+++ contracts/token/LibertyToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.17;
 
 import "../zeppelin/token/StandardToken.sol";
+import "../zeppelin/token/BurnableToken.sol";
 
-
-contract LibertyToken is StandardToken {
+contract LibertyToken is StandardToken, BurnableToken {
     string public name = "LibreBank";
     string public symbol = "LBRS";
     uint256 public decimals = 18;
@@ -12,4 +12,4 @@ contract LibertyToken is StandardToken {
         totalSupply_ = 100 * (10**6) * (10**decimals);
         balances[msg.sender] = totalSupply_;
     }
-}   
+} 
