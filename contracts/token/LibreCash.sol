0edefbafa1df0ccfd640a3a3d1fe34140a69f445
decodemybrain <decodemybrain@protonmail.ch>
2018-01-31 16:07:43 +0300

    Change ticker, delete token fallback

--- contracts/token/LibreCash.sol
+++ contracts/token/LibreCash.sol
@@ -10,17 +10,7 @@ import "../zeppelin/token/BurnableToken.sol";
  * @dev ERC20 token contract.
  */
 contract LibreCash is MintableToken, BurnableToken {
-    string public constant name = "LibreCash Token";
-    string public constant symbol = "LCT";
+    string public constant name = "LibreCash";
+    string public constant symbol = "Libre";
     uint32 public constant decimals = 18;
-
-    /**
-    * @dev Reject all ERC23 compatible tokens
-    * @param from_ The address that is transferring the tokens
-    * @param value_ the amount of the specified token
-    * @param data_ The data passed from the caller.
-    */
-    function tokenFallback(address from_, uint256 value_, bytes data_) external {
-        revert();
-    }
 }
