f9c155e7e2047b7387fecac4a994398b77c98311
decodemybrain <decodemybrain@protonmail.ch>
2018-02-06 11:42:31 +0300

    Append silent version of mock oracle

---
+++ contracts/oracles/mock/OracleMockSilent.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.17;
+import "./OracleMockBase.sol";
+
+/**
+ * @title Mocked silent oracle contract for testing purposes.
+ */
+contract OracleSilent is OracleMockBase {
+    function OracleMockTest() {
+        oracleName = "Silent Oracle";
+        rate = 0;
+    }
+
+    function updateRate() external returns(bool) {
+        updateTime = now;
+        waitQuery = true;
+        return true;
+    }
+}
