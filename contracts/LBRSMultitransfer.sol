a5023d412e83abc54f1a4b1dc3fd5c4054cc5e4e
decodemybrain <decodemybrain@protonmail.ch>
2018-02-09 13:40:40 +0300

    Append LBRS multitransfer contract.

---
+++ contracts/LBRSMultitransfer.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+import "./token/LibertyToken.sol"; 
+import "./zeppelin/ownership/Ownable.sol";
+
+contract LBRSMultitransfer is Ownable {
+    address lbrsToken;
+    LibertyToken token;
+
+    /**
+     * @dev Implements transfer method for multiple recipient. Needed in LBRS token distribution process after ICO
+     * @param recipient - recipient addresses array
+     * @param balance - refill amounts array
+     */
+    function multiTransfer(address[] recipient,uint256[] balance) public onlyOwner {
+        require(recipient.length == balance.length);
+        
+        for(uint256 i = 0; i < recipient.length; i++) {
+            token.transfer(recipient[i],balance[i]);
+        }
+    }
+
+    /**
+     * @dev Constructor
+     * @param LBRS - LBRS token address
+     */
+    function LBRSMultitransfer(address LBRS) public {
+        lbrsToken = LBRS;
+        token = LibertyToken(token);
+    }
+
+    /**
+     * @dev Returns LBRS token balance of contract.
+     */
+    function tokenBalance() public returns(uint256) {
+        return token.balanceOf(this);
+    }
+}
