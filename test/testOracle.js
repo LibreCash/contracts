404a6b64c9fb04ed126d995cbd44a282623a75d3
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2018-02-13 14:30:55 +0300

    delete don't use tests

---
+++ test/testOracle.js
@@ -0,0 +1,62 @@
+var ComplexExchanger = artifacts.require("ComplexExchanger");
+
+var oracles = [];
+[
+    "OracleMockLiza",
+    "OracleMockSasha",
+    "OracleMockKlara",
+    "OracleMockTest",
+    //"OracleMockRandom"
+].forEach( (filename) => {
+    oracles.push(artifacts.require(filename));
+});
+ 
+contract('OracleI', async function(accounts) {
+    var owner = accounts[0];
+    var acc1  = accounts[1];
+
+    it('setBank', async function() {
+        let oracle = await OracleMockTest.deployed();
+
+        try {
+            await oracle.setBank(owner);
+        } catch(e) {
+            throw new Error("Owner dont set Bank!!");
+        }
+
+        try {
+            await oracle.setBank(owner, {from:acc1});
+        } catch(e) {
+            return true;
+        }
+        throw new Error("Not Owner set Bank!!");
+    });
+
+    it('updateRate', async function() {
+        let oracle = await oracles[0].deployed();
+
+        await oracle.setBank(owner);
+        try {
+            await oracle.updateRate();
+        } catch(e) {
+            throw new Error("Owner dont update rate!!");
+        }
+
+        try {
+            await oracle.updateRate({from:acc1});
+        } catch(e) {
+            return true;
+        }
+        throw new Error("Not Bank update Rate!!");
+    });
+
+    it("get rate", async function() {
+        let oracle = await oracles[0].deployed();
+
+        await oracle.setBank(owner);
+        await oracle.updateRate();
+        
+        let rate = + await oracle.rate.call();
+        assert.equal(rate, 320000, "don't get rate");
+    });
+});
