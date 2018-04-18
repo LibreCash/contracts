6232ce836de7c067632c3705c1097949ae336e61
decodedbrain <decodemybrain@protonmail.ch>
2018-04-18 11:57:45 +0300

    Fix compiler warnings

--- contracts/oracles/mock/OracleMockLiza.sol
+++ contracts/oracles/mock/OracleMockLiza.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.18;
 import "./OracleMockBase.sol";
 
+/**
+ * @title Mocked oracles contract for testing purposes.
+ */
 contract OracleMockLiza is OracleMockBase {
-    function OracleMockLiza() {
+    function OracleMockLiza() public {
         oracleName = "Liza (Mocked Oracle, 320000)";
         mockRate = 320000;
     }
